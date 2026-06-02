b5c64d85be2db0b353e948c6c00e4d7728ea4235
hmerritt <harry@mrrtt.me>
2026-06-02 18:58:12 +0800

    chore: update explorer

--- Formula/explorer.rb
+++ Formula/explorer.rb
@@ -1,29 +1,57 @@
 class Explorer < Formula
+  release_version = "0.3.0"
+  base_url = "https://releases.mrrtt.me/explorer"
+
   desc "File Explorer"
   homepage "https://github.com/hmerritt/explorer"
-  version "0.2.0"
+  version release_version
 
   if OS.mac? && Hardware::CPU.arm?
-    url "https://releases.mrrtt.me/explorer/0.2.0/explorer-0.2.0-macos-arm64-apple-silicon.zip"
-    sha256 "7787cfea344300ed318139d409a8948432e3e8100f32fa8cc96fc214b5677e8e"
+    url "#{base_url}/#{release_version}/explorer-#{release_version}-macos-arm64-apple-silicon.zip"
+    sha256 "ab4e660d1ce195acce880fd9408fc57fc79c901f021836f3c80d11afd3c62f66"
   end
 
   if OS.mac? && Hardware::CPU.intel?
-    url "https://releases.mrrtt.me/explorer/0.2.0/explorer-0.2.0-macos-amd64-intel.zip"
-    sha256 "4442916499ad85ffae8678ea2d85e68179b761b3248cb14f80da9d05d96b41b0"
+    url "#{base_url}/#{release_version}/explorer-#{release_version}-macos-amd64-intel.zip"
+    sha256 "869ff286a52dbd8fdfd47fc74bdfea478d8cdcf9b01f3a1c5c174883b8c38c06"
   end
 
   if OS.linux? && Hardware::CPU.arm?
-    url "https://releases.mrrtt.me/explorer/0.2.0/explorer-0.2.0-linux-arm64.zip"
-    sha256 "f085cfaa60775edf76cee46192bacf08634aabf9c6989dc7e05ec8f599095c25"
+    url "#{base_url}/#{release_version}/explorer-#{release_version}-linux-arm64.tar.gz"
+    sha256 "1ffdfc1b0877285bd84e831e97cab42caa770f8da97f6e9d823aeb04c6802e15"
   end
 
   if OS.linux? && Hardware::CPU.intel?
-    url "https://releases.mrrtt.me/explorer/0.2.0/explorer-0.2.0-linux-amd64.zip"
-    sha256 "d46acc85821a3f536fd1e21a2c4801f2a33dcce0ab73663ef39c725c70ef2071"
+    url "#{base_url}/#{release_version}/explorer-#{release_version}-linux-amd64.tar.gz"
+    sha256 "cf65c742d9b79c2699e2438631f102e4c82f5fa597ef0375f24aa48c3a5b8401"
   end
 
   def install
-    bin.install "explorer"
+    if OS.linux?
+      libexec.install "explorer.app"
+
+      bin.write_exec_script libexec/"explorer.app/bin/explorer"
+
+      (share/"applications").install libexec/"explorer.app/share/applications/com.hmerritt.explorer.desktop"
+      (share/"icons/hicolor/512x512/apps").install libexec/"explorer.app/share/icons/hicolor/512x512/apps/explorer.png"
+
+      inreplace share/"applications/com.hmerritt.explorer.desktop" do |s|
+        s.gsub!(/^Exec=.*/, "Exec=#{bin}/explorer %F")
+        s.gsub!(/^Icon=.*/, "Icon=#{share}/icons/hicolor/512x512/apps/explorer.png")
+      end
+    else
+      bin.install "explorer"
+    end
+  end
+
+  test do
+    assert_predicate bin/"explorer", :exist?
+
+    if OS.linux?
+      assert_predicate libexec/"explorer.app/bin/explorer", :exist?
+      assert_predicate libexec/"explorer.app/bin/explorer.bin", :exist?
+      assert_predicate share/"applications/com.hmerritt.explorer.desktop", :exist?
+      assert_predicate share/"icons/hicolor/512x512/apps/explorer.png", :exist?
+    end
   end
 end
