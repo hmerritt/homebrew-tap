28ca994def404f09fcb843626f01c5c4339a811e
hmerritt <harry@mrrtt.me>
2026-07-11 13:51:12 +0100

    chore: update auto-extract

--- Formula/auto-extract.rb
+++ Formula/auto-extract.rb
@@ -1,5 +1,5 @@
 class AutoExtract < Formula
-  release_version = "1.4.102"
+  release_version = "1.4.103"
   base_url = "https://releases.mrrtt.me/auto-extract"
 
   desc "Watches your Downloads directory (or any directory) for ZIPs and archives and automatically extracts them for you"
@@ -8,22 +8,22 @@ class AutoExtract < Formula
 
   if OS.mac? && Hardware::CPU.arm?
     url "#{base_url}/v#{release_version}/auto-extract_#{release_version}_darwin_arm64.zip"
-    sha256 "562d2b771a2bf450cbf2d91dbf8d1ac8896a3737c5f8e24dd614b94b185aaba0"
+    sha256 "3fdb7823ee4286ddbb06ceae3a6ba549f82fa0c90fc03d2d14e672878238ad8e"
   end
 
   if OS.mac? && Hardware::CPU.intel?
     url "#{base_url}/v#{release_version}/auto-extract_#{release_version}_darwin_amd64.zip"
-    sha256 "a695940550f5ab86a8d48e16c2cbc49d1350168314519fc6ecda46d707d30fd8"
+    sha256 "9124fbbceff61e4b7c6080ba0524abc98d34e2da8da0e4ab26c907f8bccb59b0"
   end
 
   if OS.linux? && Hardware::CPU.arm?
     url "#{base_url}/v#{release_version}/auto-extract_#{release_version}_linux_arm64.zip"
-    sha256 "2cd27e019bde753ee0470e0e0812fa23e7508989e7f76ed789a6deebae6be3a6"
+    sha256 "d7f4bf1cd58aaa380fbfef87f8faa2ca9dcfc742ea89e3ecd4445e91fb7babb9"
   end
 
   if OS.linux? && Hardware::CPU.intel?
     url "#{base_url}/v#{release_version}/auto-extract_#{release_version}_linux_amd64.zip"
-    sha256 "4129f5803f027d5da4e23adabf9699693f0c5e232d34b03bf6e07ff24dfd4dd5"
+    sha256 "4bd733f8697b07caa68518e51ca071a7f1ea7922b8989300d8339eeac36187cf"
   end
 
   def install
