d717c7c07e2d7ad6436db2f08ce1db309bb5901b
hmerritt <harry@mrrtt.me>
2026-07-11 12:39:16 +0100

    chore: update auto-extract

--- Formula/auto-extract.rb
+++ Formula/auto-extract.rb
@@ -1,26 +1,29 @@
 class AutoExtract < Formula
+  release_version = "1.3.100"
+  base_url = "https://releases.mrrtt.me/auto-extract"
+
   desc "Watches your Downloads directory (or any directory) for ZIPs and archives and automatically extracts them for you"
   homepage "https://gitlab.com/mrrttjd/auto-extract"
-  version "1.2.97"
+  version release_version
 
   if OS.mac? && Hardware::CPU.arm?
-    url "https://releases.mrrtt.me/auto-extract/v1.2.97/auto-extract_1.2.97_darwin_arm64.zip"
-    sha256 "93639c3bd692ecda7e97a436e2aee877743caf5d88d805bb68509a449a011397"
+    url "#{base_url}/v#{release_version}/auto-extract_#{release_version}_darwin_arm64.zip"
+    sha256 "16b1da4e778ec4d1ea4a3eb86c7b5f04f8848e31de017a37c274a4558d731088"
   end
 
   if OS.mac? && Hardware::CPU.intel?
-    url "https://releases.mrrtt.me/auto-extract/v1.2.97/auto-extract_1.2.97_darwin_amd64.zip"
-    sha256 "57f7b7c999e87012ce0d9245128cfc33f2eaa613202f9703b4a7b145dba46e93"
+    url "#{base_url}/v#{release_version}/auto-extract_#{release_version}_darwin_amd64.zip"
+    sha256 "cfb5f2c173b94f222a86515d68fdedde88e58c6ad8dd177ded90b7db57696c5e"
   end
 
   if OS.linux? && Hardware::CPU.arm?
-    url "https://releases.mrrtt.me/auto-extract/v1.2.97/auto-extract_1.2.97_linux_arm64.zip"
-    sha256 "0887adaca8cc0adf2ae460a3c255bfe72b30b716b0fdfe23e12dfb0ace4c0229"
+    url "#{base_url}/v#{release_version}/auto-extract_#{release_version}_linux_arm64.zip"
+    sha256 "be038f56d6ffa243bf2f5c1502f3dd42f84ea34758dd933bc7ab324e70ae1016"
   end
 
   if OS.linux? && Hardware::CPU.intel?
-    url "https://releases.mrrtt.me/auto-extract/v1.2.97/auto-extract_1.2.97_linux_amd64.zip"
-    sha256 "5161c5db02eb14366b04ee46c24c2d1b69384cc1d2b6a24d6f7a639e0fccf579"
+    url "#{base_url}/v#{release_version}/auto-extract_#{release_version}_linux_amd64.zip"
+    sha256 "47a5bfdbf590c318c80b374b89d5d148b9e7653c75ff315b0eadb0ac481e4a20"
   end
 
   def install
